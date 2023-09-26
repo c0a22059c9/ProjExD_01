3435fa5a741450fe1d6fb33f7dca143a8ea2774d
koide <c0a22059c9@edu.teu.ac.jp>
2023-09-26 15:27:01 +0900

    こうかとん

--- pygame_image.py
+++ pygame_image.py
@@ -7,12 +7,16 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bird_img = pg.image.load("ex01/fig/3.png")
+    bird_img = pg.transform.flip(bird_img,True,False)
+    bird_img = pg.transform.rotozoom(bird_img,10,1.0)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(bird_img,[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
