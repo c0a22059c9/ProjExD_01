78a8f36faa3423258417d14f52c1f86ee4408e09
koide <c0a22059c9@edu.teu.ac.jp>
2023-09-26 17:03:56 +0900

    こうかとん

--- pygame_image.py
+++ pygame_image.py
@@ -10,7 +10,7 @@ def main():
     bg_img2 = pg.transform.flip(bg_img,True,False)
     bird_img = pg.image.load("ex01/fig/3.png")
     bird_img = pg.transform.flip(bird_img,True,False)
-    bird_img2 = pg.transform.rotozoom(bird_img,1,1.0)
+    bird_img2 = pg.transform.rotozoom(bird_img,5,1.0)
     bird_imgs = [bird_img,bird_img2]
     tmr = 0
     while True:
@@ -18,13 +18,17 @@ def main():
             if event.type == pg.QUIT: return
 
         x = tmr%3200
+        if tmr%100 <= 50:
+            num = 0
+        else:
+            num = 1
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [1600-x, 0])
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(bird_imgs[tmr%2],[300,200])
+        screen.blit(bird_imgs[num],[300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(100)
+        clock.tick(1000)
 
 
 if __name__ == "__main__":
