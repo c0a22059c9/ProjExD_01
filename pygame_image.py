2649ccee5a2d8ac8f966750d64333ed8c603688b
koide <c0a22059c9@edu.teu.ac.jp>
2023-09-26 15:42:18 +0900

    こうかとん

--- pygame_image.py
+++ pygame_image.py
@@ -4,21 +4,21 @@ import pygame as pg
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((1600, 900))
+    screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     bird_img = pg.image.load("ex01/fig/3.png")
     bird_img = pg.transform.flip(bird_img,True,False)
     bird_img2 = pg.transform.rotozoom(bird_img,10,1.0)
+    bird_imgs = [bird_img,bird_img2]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(bird_img,[300,200])
-        screen.blit(bg_img, [0, 0])
-        screen.blit(bird_img2,[300,200])
+        screen.blit(bird_imgs[0],[300,200])
+        screen.blit(bird_imgs[1],[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
